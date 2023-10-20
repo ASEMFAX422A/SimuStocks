a7dbdc3aed25845aaa7e8f726e25cbb78239595b
vonrodalars <lars@von-roda.de>
2023-10-20 11:52:56 +0200

    start import different packages and Configuring dirffrent run modes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
+import json
+import logging
+import os
+from flask import Flask, session, request, g, redirect, url_for, render_template
+from flask_mongoengine import MongoEngine, MongoEngineSessionInterface
+from flask_socketio import SocketIO, disconnect
+from flask_wtf import CSRFProtect
+from logging import StreamHandler, Formatter
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
+
+configs = {
+    'dev': 'config_dev',
+    'test': 'config_test',
+    'prod': 'config_prod'
+}
+
+mode = os.environ.get('SIMU_MODE', 'dev')
+app.config.from_object(configs[mode])
+
+db = MongoEngine(app)
+app.session_interface = MongoEngineSessionInterface(db)
 
 
 @app.route('/')
