19da323d6f509e76252eaba520b1242e900955a9
vonrodalars <94010633+vonrodalars@users.noreply.github.com>
2023-10-23 16:51:57 +0200

    adding some functionalitys to produce logs.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 from flask import Flask, session, request, g, redirect, url_for, render_template
+from flask_login import LoginManager, AnonymousUserMixin, current_user
 from flask_mongoengine import MongoEngine, MongoEngineSessionInterface
 from flask_socketio import SocketIO, disconnect
 from flask_wtf import CSRFProtect
@@ -22,6 +23,8 @@ app.config.from_object(configs[mode])
 db = MongoEngine(app)
 app.session_interface = MongoEngineSessionInterface(db)
 
+login_manager = LoginManager()
+
 from .models import *
 
 current_run = AppRun(version=app.config["VERSION"],
@@ -29,5 +32,33 @@ current_run = AppRun(version=app.config["VERSION"],
                      mode=app.config['SIMU_MODE'])
 current_run.save()
 
+
+class Anonymous(AnonymousUserMixin):
+    def __init__(self):
+        self.name = "Anonymous User"
+
+
+class MongoHandler(StreamHandler):
+    def __init__(self):
+        StreamHandler.__init__(self)
+
+    def emit(self, record):
+        time = datetime.fromtimestamp(record.created)
+        ApplicationLog(time=time,
+                       levelname=record.levelname,
+                       module=record.module,
+                       msg=record.msg,
+                       appRun=current_run).save()
+
+
+mongologger = MongoHandler()
+mongologger.setLevel(logging.NOTSET)
+mongologger.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+app.logger.addHandler(mongologger)
+
+login_manager.init_app(app)
+login_manager.login_view = "login"
+login_manager.login_message = None
+login_manager.anonymous_user = Anonymous
 
 from . import controllers
--- app/controllers.py
+++ app/controllers.py
@@ -1,5 +1,64 @@
-from app import app
-from flask import render_template
+from app import app, login_manager
+
+
+from flask_login import login_user, logout_user, current_user, login_required
+from flask import render_template, redirect, url_for
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.objects(id=userid).first()
+
+
+@app.route("/produce_debug")
+def produce_debug():
+    if current_user.is_authenticated:
+        app.logger.debug("Debug was produced by %s" % current_user.full_name)
+        # current_user.full_name requires we have the function in User model to return full_name
+    else:
+        app.logger.debug("Debug was produced by Anonymous")
+    return redirect(url_for('index'))
+
+
+@app.route("/produce_info")
+def produce_info():
+    if current_user.is_authenticated:
+        app.logger.info("Info was produced by %s" % current_user.full_name)
+        # current_user.full_name requires we have the function in User model to return full_name
+    else:
+        app.logger.info("Info was produced by Anonymous")
+    return redirect(url_for('index'))
+
+
+@app.route("/produce_warning")
+def produce_warning():
+    if current_user.is_authenticated:
+        app.logger.warning("Warning was produced by %s" % current_user.full_name)
+        # current_user.full_name requires we have the function in User model to return full_name
+    else:
+        app.logger.warning("Warning was produced by Anonymous")
+    return redirect(url_for('index'))
+
+
+@app.route("/produce_error")
+def produce_error():
+    if current_user.is_authenticated:
+        app.logger.error("Error was produced by %s" % current_user.full_name)
+        # current_user.full_name requires we have the function in User model to return full_name
+    else:
+        app.logger.error("Error was produced by Anonymous")
+    return redirect(url_for('index'))
+
+
+@app.route("/produce_critical")
+def produce_critical():
+    if current_user.is_authenticated:
+        app.logger.critical("CRITICAL was produced by %s" % current_user.full_name)
+        # current_user.full_name requires we have the function in User model to return full_name
+    else:
+        app.logger.critical("CRITICAL was produced by Anonymous")
+    return redirect(url_for('index'))
+
 
 @app.route('/')
 def index():
