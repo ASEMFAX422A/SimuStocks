66518299149003752f68368aa3d9aca215ab391b
vonrodalars <94010633+vonrodalars@users.noreply.github.com>
2023-10-23 17:01:58 +0200

    add error.html, template for all Errors, _main_simple.html, the simple site without nav/topbar to simplifi errorsites and other pages. Also adding some errorhandlers

--- app/controllers.py
+++ app/controllers.py
@@ -2,7 +2,7 @@ from app import app, login_manager
 
 
 from flask_login import login_user, logout_user, current_user, login_required
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 
 
 @login_manager.user_loader
@@ -63,3 +63,37 @@ def produce_critical():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.errorhandler(401)
+def http_error_unauthorized(e):
+    return render_template("errors/error.html"), 401
+
+
+@app.errorhandler(403)
+def http_error_forbidden(e):
+    return render_template("errors/error.html"), 403
+
+
+@app.errorhandler(404)
+def http_error_page_not_found(e):
+    if current_user.is_authenticated:
+        app.logger.info("%s tryed to Request %s but this doesn't exist" % (current_user, request.path))
+    else:
+        app.logger.info("unknown User tryed to Request %s but this doesn't exist" % request.endpoint)
+    return render_template('errors/error.html'), 404
+
+
+@app.errorhandler(500)
+def http_error_internal_server_error(e):
+    return render_template('errors/error.html'), 500
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    if current_user.is_authenticated:
+        app.logger.error("%s created UNEXPEDTED ERROR: %s" % (current_user, e))
+    else:
+        app.logger.error("unknown User created UNEXPEDTED ERROR: %s" % e)
+    return http_error_internal_server_error(e)
+
