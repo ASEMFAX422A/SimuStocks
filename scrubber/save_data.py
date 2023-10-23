d9eaae27bf28fd5e41d8abba595afd3d0c210fd6
vonrodalars <94010633+vonrodalars@users.noreply.github.com>
2023-10-23 22:37:38 +0200

    Scrubber can now save to DB

--- scrubber/save_data.py
+++ scrubber/save_data.py
@@ -1,7 +1,41 @@
-# save_data.py
+import os
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from datetime import datetime
+
+# Globale Variable für die MongoDB-Verbindung
+db_client = None
+
+
+def connect_to_db():
+    global db_client
+    if db_client is None:
+        try:
+            mongo_user = os.environ.get("MONGODB_USERNAME")
+            mongo_pass = os.environ.get("MONGODB_PASSWORD")
+            mongo_host = os.environ.get("MONGODB_HOSTNAME")
+            mongo_db = os.environ.get("MONGODB_DATABASE")
+
+            db_client = MongoClient(f"mongodb://{mongo_user}:{mongo_pass}@{mongo_host}:27017/{mongo_db}",
+                                    maxPoolSize=50)[mongo_db]
+        except ConnectionFailure:
+            print("Could not connect to MongoDB, retrying...")
+            db_client = None
+
 
 def save_data(price, ticker):
-    file_name = f"{ticker}_stock_prices.txt"
-    with open(file_name, 'a') as f:
-        f.write(f"{price}\n")
+    if db_client is None:
+        connect_to_db()  # Versucht, die Verbindung herzustellen oder wiederherzustellen
+    if db_client is not None:
+        collection = db_client['Prices']
+        timestamp = datetime.utcnow().isoformat()
 
+        collection.update_one(
+            {'ticker': ticker},
+            {'$push': {'data': {'price': price, 'timestamp': timestamp}}},
+            upsert=True
+        )
+
+
+# Verbindung beim Start der Anwendung initialisieren
+connect_to_db()
--- scrubber/scrubbedy.py
+++ scrubber/scrubbedy.py
@@ -9,18 +9,16 @@ def create_job(ticker):
         price = get_stock_price(ticker)
         save_data(price, ticker)
         print(f"Saved {ticker} stock price: {price}")
-    return job
 
+    return job
 
 if __name__ == "__main__":
     tickers = ["AAPL", "MSFT", "GOOGL", "EOAN.DE"]
 
     for ticker in tickers:
         job = create_job(ticker)
-        # Plan the job to run once a day at a specific time
-        schedule.every().day.at("21:47").do(job)
+        schedule.every(15).minutes.do(job)
 
-    # Keep the script running
     while True:
         schedule.run_pending()
         time.sleep(1)
