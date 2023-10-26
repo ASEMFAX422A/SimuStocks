7cf3ba552c6ce9e8f158382b8383240ef029fc20
vonrodalars <94010633+vonrodalars@users.noreply.github.com>
2023-10-26 15:37:43 +0200

    adding Test for function is_valid_object_id in app.utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import Mock
-from SimuApp.app.utils import safe_cast
+from SimuApp.app.utils import safe_cast, is_valid_object_id
 
 class TestUtils(unittest.TestCase):
 
@@ -13,3 +13,11 @@ class TestUtils(unittest.TestCase):
 
         result = safe_cast('abc', int, self.mock_app, default=0)
         self.assertEqual(result, 0)
+
+    def test_valid_object_id(self):
+        self.assertTrue(is_valid_object_id("5f5b5f7e1c9d440000c7877e"))
+        self.assertFalse(is_valid_object_id("invalid_id"))
+        self.assertFalse(is_valid_object_id(""))
+        self.assertFalse(is_valid_object_id(None))
+        self.assertFalse(is_valid_object_id("5f5b5f7e1c9d440000c78"))
+
