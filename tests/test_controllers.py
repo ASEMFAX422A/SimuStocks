1d35ea3349791585102c7ba2fd45825f6565b95e
vonrodalars <94010633+vonrodalars@users.noreply.github.com>
2023-10-26 15:44:16 +0200

    Fixing test_controllers

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -1,14 +1,19 @@
-# SimuApp/tests/test_controllers.py
-import unittest
+# /tests/test_controllers.py
+
+from SimuApp.app import create_app  # Pfad zur create_app Funktion
 from flask import url_for
-from SimuApp.app import create_app
+import unittest
+
 
 class TestControllers(unittest.TestCase):
 
     def setUp(self):
         self.app = create_app()
+        self.app.config['SERVER_NAME'] = 'localhost:5000'
         self.client = self.app.test_client()
 
     def test_index_route(self):
-        response = self.client.get(url_for('main.index'))
+        with self.app.app_context():
+            response = self.client.get(url_for('main.index'))
         self.assertEqual(response.status_code, 200)
+
