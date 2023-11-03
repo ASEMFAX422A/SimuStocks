00dd0379c3e907f7c7a1d41ea6223469bc5d8d1a
vonrodalars <lars@von-roda.de>
2023-11-03 10:21:39 +0100

    add autorun

--- SimuApp/config_dev.py
+++ SimuApp/config_dev.py
@@ -5,11 +5,11 @@ from config_default import *
 
 SIMU_MODE = "dev"
 
-MONGODB_DB = "flaskdb"
-MONGODB_HOST = "mongodb"
-MONGODB_USERNAME = "mongodbuser"
-MONGODB_PASSWORD = "your_mongodb_root_password"
-MONGODB_PORT = 27017
+MONGODB_DB = os.environ.get("MONGODB_DB", "flaskdb")
+MONGODB_HOST = os.environ.get("MONGODB_HOSTNAME", "mongodb")
+MONGODB_USERNAME = os.environ.get("MONGODB_USERNAME", "mongodbuser")
+MONGODB_PASSWORD = os.environ.get("MONGODB_PASSWORD", "your_mongodb_root_password")
+MONGODB_PORT = int(os.environ.get("MONGODB_PORT", 27017))
 
 DEBUG = True
 
