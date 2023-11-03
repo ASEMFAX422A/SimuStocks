f3bdcec1501228295d7f79cf6e534707e665cc9c
h0useofdupree <joel.riekemann@gmail.com>
2023-11-03 12:39:43 +0100

    Move listings rout to c_listings.py

--- SimuApp/app/c_listings.py
+++ SimuApp/app/c_listings.py
@@ -1,9 +1,22 @@
 # SimuApp/app/stocks/listings.py
 from .models import *
 from flask import current_app as app
-from flask import Blueprint, render_template, redirect, url_for, request
-from flask_login import login_user, logout_user, current_user, login_required
+from flask import (
+    Blueprint,
+    render_template,
+    redirect,
+    url_for,
+    request,
+    session,
+    abort,
+    flash,
+)
 
 listings = Blueprint("c_listings", __name__)
 
 login_manager = None
+
+
+@listings.route("/stocks/listings")
+def listings():
+    return render_template("stocks/listings.html")
