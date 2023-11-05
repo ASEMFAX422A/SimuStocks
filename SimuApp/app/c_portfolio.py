8fd9329df446e7e495889645b62acd59fcbd3796
h0useofdupree <joel.riekemann@gmail.com>
2023-11-05 13:32:11 +0100

    fixed routes

---
+++ SimuApp/app/c_portfolio.py
@@ -0,0 +1,22 @@
+# SimuApp/app/c_portfolio.py
+from .models import *
+from flask import current_app as app
+from flask import (
+    Blueprint,
+    render_template,
+    redirect,
+    url_for,
+    request,
+    session,
+    abort,
+    flash,
+)
+
+c_portfolio = Blueprint("c_portfolio", __name__)
+
+login_manager = None
+
+
+@c_portfolio.route("/portfolio")
+def portfolio():
+    return render_template("pages/stocks/portfolio.html")
