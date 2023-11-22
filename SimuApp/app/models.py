7d2f1b366f0ec126ad2807cd45243c22436349ca
Rhoqaa <70476216+Rhoqaa@users.noreply.github.com>
2023-11-22 21:56:51 +0100

    Revert "Hinzufügen von DataEntry und Aktualisieren models und c_listings"
    This reverts commit 98e7713c63528f2da21441012864dfe704056e11.

--- SimuApp/app/models.py
+++ SimuApp/app/models.py
@@ -1,19 +1,24 @@
 # SimuApp/app/models.py
 import cryptocode
+
 from flask import current_app as app
 from . import db
-from datetime import datetime
+
+from datetime import datetime, timedelta, date, time
 from flask_mongoengine import BaseQuerySet
 from flask_login import UserMixin, current_user
 
+
 class TimestampedModel(object):
     """Mixin for keeping track of created and last modified times"""
+
     created = db.DateTimeField(default=datetime.now)
     modified = db.DateTimeField(default=datetime.now)
 
+
 class SimuBaseModell:
     enabled = db.BooleanField(default=True)
-    
+
     @classmethod
     def column_names(cls):
         return [k for k in cls._fields]
@@ -22,53 +27,66 @@ class SimuBaseModell:
     def public_id(self):
         return cryptocode.encrypt(str(self.id), app.config["PUBLIC_ID_KEY"])
 
-class DataEntry(db.EmbeddedDocument):
-    # Embedded document for storing price and timestamp data
-    price = db.FloatField()
-    timestamp = db.DateTimeField(default=datetime.now)
 
 class ObjectChangelog(db.EmbeddedDocument, SimuBaseModell):
-    # Embedded document for storing object changelog data
     time = db.DateTimeField(default=datetime.now)
     modified_by = db.ReferenceField("User")
     note = db.StringField()
     data = db.DictField()
 
-class Stocks(db.DynamicDocument):
-    # Collection for storing stock data
-    meta = {"collection": "Prices"}
-    ticker = db.StringField(required=True)
-    data = db.ListField(db.EmbeddedDocumentField(DataEntry))
 
 class AppRun(db.DynamicDocument):
-    # Collection for storing application run data
     meta = {"collection": "app_run"}
     time = db.DateTimeField(default=datetime.now)
-    version = db.StringField(required=True)
-    version_id = db.StringField(required=True)
-    mode = db.StringField(required=True)
+
+    version = db.StringField(
+        required=True
+    )  # Like 1.0.0-DEV-202012201200 (the last is date and time)
+    # Format of the date is YYYY MM DD HH MM SS without blanks
+
+    version_id = db.StringField(required=True)  # Version Like 1.0.0
+
+    mode = db.StringField(required=True)  # The App mode, like DEV, PROD, TEST.
+    # Typically you can get this information already form the Database
+
 
 class ApplicationLog(db.DynamicDocument):
-    # Collection for storing application log data
     meta = {"collection": "application_logs"}
-    time = db.DateTimeField(default=datetime.now)
-    levelname = db.StringField(required=True)
-    module = db.StringField(required=True)
-    msg = db.StringField(required=True)
+    time = db.DateTimeField(default=datetime.now)  # created
+
+    levelname = db.StringField(required=True)  # The levelname of the Log.
+    # See https://docs.python.org/3/howto/logging.html#logging-levels for more information
+
+    module = db.StringField(required=True)  # module
+    msg = db.StringField(required=True)  # msg
     appRun = db.ReferenceField("AppRun", required=True)
 
+
+class Stocks(db.DynamicDocument):
+    meta = {"collection": "Prices"}
+
+    ticker = db.StringField(required=True)
+    data = db.ListField()
+
+
+class DataEntry(db.EmbeddedDocument):
+    price = db.FloatField()
+    timestamp = db.StringField()
+
+
 class Prices(db.DynamicDocument):
-    # Collection for storing price data
     _id = db.StringField(primary_key=True)
     ticker = db.StringField()
     data = db.ListField(db.EmbeddedDocumentField(DataEntry))
 
+
 class User(UserMixin, db.Document, SimuBaseModell):
-    # User collection for storing user data
     firstname = db.StringField(required=True)
     lastname = db.StringField(required=True)
     email = db.EmailField(required=True, unique=True)
+
     login_times = db.ListField(db.DateTimeField(default=datetime.now))
+
     enabled = db.BooleanField(default=True)
     changelog = db.EmbeddedDocumentListField(ObjectChangelog)
 
